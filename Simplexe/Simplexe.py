38dff3da60d486a77f03f9365228bb04afdb004b
E202881Y <E202881Y@etu.univ-nantes.fr>
2024-11-19 02:34:54 +0100

    Luc

--- Simplexe/Simplexe.py
+++ Simplexe/Simplexe.py
@@ -1,224 +1,3 @@
-# from typing import List, Optional
-
-# #! Ne pas oublié de parler de la complexité de cette algorithme Simplexe
-
-# # Fonction pour résoudre un problème d'optimisation linéaire Simplexe
-# # coef: Coefficients de la fonction objectif
-# # matrice_sous_contrainte: Matrice des contraintes
-# # vecteur: Côtés droits des inégalités
-# # type_inegalites: Type des inégalités
-# # maximisation: Maximiser ou minimiser (Sachant qu'on ne vas pas implémenter la minimisation)
-# def simplexe(
-#     coef: List[float],
-#     matrice_sous_contrainte: List[List[float]],
-#     vecteurs_Q,
-#     type_inegalite,
-#     maximisation=True,
-# ):
-#     Z = 0
-
-#     # Création de la matrice des coefficients du bloc des contraintes
-#     matrice_coef_bloc_contrainte = matrice_sous_contrainte.copy()
-
-#     # Ajout des colonnes pour les variables d'écart
-#     for row in matrice_coef_bloc_contrainte:
-#         for _ in range(len(matrice_sous_contrainte)):
-#             row.append(0)
-
-#     # Remplissage des colonnes des variables d'écart
-#     for row_index in range(len(matrice_sous_contrainte)):
-#         if type_inegalite[row_index] == "<=":
-#             # On ajoute 1 pour les variables d'écart si c'est une inégalité <=
-#             matrice_coef_bloc_contrainte[row_index][
-#                 row_index + len(matrice_sous_contrainte)
-#             ] = 1
-#         else:
-#             # On ajoute -1 pour les variables d'écart si c'est une inégalité >=
-#             matrice_coef_bloc_contrainte[row_index][
-#                 row_index + len(matrice_sous_contrainte)
-#             ] = -1
-
-#     # Ajout des colonnes pour les variables d'écart
-#     for _ in range(len(matrice_sous_contrainte)):
-#         coef.append(0)
-
-#     # Création du vecteur Zj
-#     Zj = [0 for _ in range(len(coef))]
-
-#     # Création du vecteur Cj - Zj
-#     difference_Cj_Zj = [0 for _ in range(len(coef))]
-
-#     # Création du vecteur des coefficients pivot
-#     coef_pivot = [0 for _ in range(len(matrice_sous_contrainte))]
-
-#     for i in range(len(coef)):
-#         difference_Cj_Zj[i] = coef[i] - Zj[i]
-
-#     valeur_entrante = max(difference_Cj_Zj)
-#     colonne_valeur_entrante = difference_Cj_Zj.index(valeur_entrante)
-
-#     # Initialisation des variables dans base
-#     vdb = ["" for _ in range(len(coef_pivot))]
-#     for i in range(len(vdb)):
-#         vdb[i] = "x" + str(i + len(coef_pivot) + 1)
-
-#     # Initialisation des ratios
-#     ratio = [0 for _ in range(len(matrice_coef_bloc_contrainte))]
-
-#     # ============== Affichage du tableau Simplexe Initialisation =================
-#     display_tab(
-#         coef,
-#         matrice_coef_bloc_contrainte,
-#         vecteurs_Q,
-#         coef_pivot,
-#         Z,
-#         Zj,
-#         difference_Cj_Zj,
-#         vdb,
-#         ratio,
-#     )
-#     # ===================== Fin de l'affichage =====================
-
-#     # Calcul de la valeur sortante
-#     min_value = vecteurs_Q[0] / matrice_coef_bloc_contrainte[0][colonne_valeur_entrante]
-#     index_min_value = 0
-
-#     ratio[0] = vecteurs_Q[0] / matrice_coef_bloc_contrainte[0][colonne_valeur_entrante]
-
-#     for i in range(1, len(matrice_coef_bloc_contrainte)):
-#         if (
-#             vecteurs_Q[i] / matrice_coef_bloc_contrainte[i][colonne_valeur_entrante]
-#             < min_value
-#         ):
-#             min_value = (
-#                 vecteurs_Q[i] / matrice_coef_bloc_contrainte[i][colonne_valeur_entrante]
-#             )
-#             index_min_value = i
-
-#         ratio[i] = (
-#             vecteurs_Q[i] / matrice_coef_bloc_contrainte[i][colonne_valeur_entrante]
-#         )
-
-#     coef_pivot[index_min_value] = min_value
-#     pivot = matrice_coef_bloc_contrainte[index_min_value][colonne_valeur_entrante]
-#     ligne_pivot = [x / pivot for x in matrice_coef_bloc_contrainte[index_min_value]]
-
-#     vecteurs_Q[index_min_value] = vecteurs_Q[index_min_value] / pivot
-
-#     matrice_coef_bloc_contrainte[index_min_value] = ligne_pivot
-
-#     # Calcul des lignes par rapport à la ligne pivot
-#     for row in range(len(matrice_coef_bloc_contrainte)):
-#         if row != index_min_value:
-#             coef_colonne_pivot = matrice_coef_bloc_contrainte[row][colonne_valeur_entrante]
-#             print("coef_colonne_pivot", coef_colonne_pivot)
-#             print("row de matrice_coef_bloc_contrainte", matrice_coef_bloc_contrainte[row])
-#             matrice_coef_bloc_contrainte[row] = [
-#                 x - coef_colonne_pivot * pivot for x in matrice_coef_bloc_contrainte[row]
-#             ]
-#             vecteurs_Q[row] = vecteurs_Q[row] - (coef_colonne_pivot * pivot)
-
-#     print("ligne pivot", ligne_pivot)
-#     print("vecteurs_Q", vecteurs_Q)
-
-#     # division par le pivot pour la ligne pivot
-
-#     # Calcul de Z
-#     Z = 0
-#     for i in range(len(coef_pivot)):
-#         Z = Z + coef_pivot[i] * vecteurs_Q[i]
-
-#     # Calcul pour trouver le nouveau Valeur Dans la Base
-#     max_difference_Cj_Zj = max(difference_Cj_Zj)
-#     print("Max difference Cj - Zj: ", max_difference_Cj_Zj)
-#     index_max_difference_Cj_Zj = difference_Cj_Zj.index(max_difference_Cj_Zj)
-#     print("Index max difference Cj - Zj: ", index_max_difference_Cj_Zj)
-
-#     vdb[index_min_value] = "x" + str(index_max_difference_Cj_Zj + 1)
-
-#     # ============== Affichage du tableau Simplexe =================
-#     display_tab(
-#         coef,
-#         matrice_coef_bloc_contrainte,
-#         vecteurs_Q,
-#         coef_pivot,
-#         Z,
-#         Zj,
-#         difference_Cj_Zj,
-#         vdb,
-#         ratio,
-#     )
-#     # ===================== Fin de l'affichage =====================
-
-#     print("Pivot: ", pivot)
-#     print(
-#         "Position pivot: row:",
-#         index_min_value + 1,
-#         "column: x",
-#         colonne_valeur_entrante + 1,
-#     )
-
-
-# # Fonction pour afficher le tableau Simplexe
-# def display_tab(
-#     coef: List[float],
-#     matrice_sous_contrainte: List[List[float]],
-#     vecteurs_Q,
-#     coef_pivot,
-#     Z,
-#     Zj,
-#     difference_Cj_Zj,
-#     vdb,
-#     ratio,
-# ):
-#     print("=================================================")
-#     print("Tableau Simplexe")
-#     print("Cj                                     |", coef, "   |  Ratio")
-#     print("-------------------------------------------------")
-#     if len(coef_pivot) == 2:
-#         print("     CP      |    vdb    |      Q      | x1 x2 x3 x4  |")
-#     else:
-#         print("     CP      |    vdb    |      Q      | x1 x2 x3 x4 x5 x6                    |")
-#     print("-------------------------------------------------")
-#     for column in range(len(coef_pivot)):
-#         print(
-#             f"{coef_pivot[column]:.2f}         | {vdb[column]}        | {vecteurs_Q[column]:.1f}        | {matrice_sous_contrainte[column]}   | {ratio[column]:.2f}"
-#         )
-#     print("Z =", Z,"       |     Zj    |", Zj)
-#     print("Cj - Zj          |", difference_Cj_Zj)
-
-
-# # =================== Testing ===================
-# # Coefficients de la fonction objectif
-# c = [4, 5, 6]
-
-# # Matrice des contraintes
-# A = [[2, 3, 1], [1, 4, 2], [3, 1, 2]]
-
-# # Côtés droits des inégalités
-# b = [10, 12, 14]
-
-# type_inegalite = ["<=", "<=", "<="]
-
-# # Appel de la fonction pour maximiser
-# print("3 Variables")
-# simplexe(c, A, b, type_inegalite, maximisation=True)
-
-# # Exemple avec 2 variables
-# c = [2, 3]
-# # Matrice des contraintes
-# A = [[1, 2], [2, 1]]
-
-# # Côtés droits des inégalités
-# b = [8, 10]
-
-# type_inegalite = ["<=", "<="]
-
-# # Appel de la fonction pour maximiser
-# # print("2 Variables")
-# # simplexe(c, A, b, type_inegalite, maximisation=True)
-
-# --------------------------------------------------------------------------------------------------------------------------------------------------
 import numpy as np
 import pandas as pd
 # Luc
@@ -275,8 +54,6 @@ def forme_standard(contraintes, inegalites) :
             contraintes_standard[lignes][lignes + nb_colonnes_init] = -1
     return contraintes_standard
 
-# tab = forme_standard(contraintes, inegalites)
-
 # Etape 2 : Initialisation des paramètres
 
 def initParam(tab, size_contraintes, objectif):
@@ -292,17 +69,11 @@ def initParam(tab, size_contraintes, objectif):
     max_Cj_Zj = max(Cj_Zj)
     return nom_coeff, cp, vdb, ratio, z, zj, Cj_Zj, max_Cj_Zj
 
-# nom_coeff, cp, vdb, ratio, select_tab_vdb, z, zj, Cj_Zj, max_Cj_Zj = initParam(tab, size_contraintes, objectif)
-
-
 def built_tab_simplex(cp, vdb, quantite, tab, nom_coeff, ratio):
     df_param = pd.DataFrame({"Cp": cp,"VDB": vdb,"Q": quantite}) #Tables de gauche
     df_coeff = pd.DataFrame(tab, columns=nom_coeff) #Matrice de coefficients
     df_ratio = pd.DataFrame({"ratio": ratio}) #Table de ratios
     return pd.concat([df_param, df_coeff, df_ratio], axis=1)
-
-
-# df_tab0 = built_tab_simplex(cp, vdb, quantite, tab, nom_coeff, ratio) 
 
 def print_tab_simplex(z,df_tab, msg, zj, Cj_Zj):
     # # Affichage du DataFrame
@@ -313,14 +84,10 @@ def print_tab_simplex(z,df_tab, msg, zj, Cj_Zj):
     print("          Cj - Zj", Cj_Zj)
     print(" ")
 
-# print_tab_simplex(df_tab0, "Tableau initial")
-
 def calcule_v_entrante(tab, max_Cj_Zj, Cj_Zj):
     index_colonne_valeur_entrante = Cj_Zj.index(max_Cj_Zj)
     colonne_pivot = [ligne[index_colonne_valeur_entrante] for ligne in tab]
     return index_colonne_valeur_entrante, colonne_pivot
-
-# index_colonne_valeur_entrante, colonne_pivot, valeur_entrante = calcule_v_entrante(tab, max_Cj_Zj, nom_coeff, Cj_Zj)
 
 # Etape 4 : Choisir la variable à enlever de la base
 
@@ -333,7 +100,7 @@ def calcule_v_sortante(tab, ratios):
     ratios_positifs = [x for x in ratios if x > 0]
     if ratios_positifs :
         min_ratio = min(ratios_positifs)
-    
+
     index_ligne_valeur_sortante = ratios.index(min_ratio)
     ligne_pivot = tab[index_ligne_valeur_sortante]
     return index_ligne_valeur_sortante, ligne_pivot
@@ -344,9 +111,6 @@ def calcule_pivot(tab, index_ligne_valeur_sortante, index_colonne_valeur_entrant
 def ratio_pivot_a_jour(ratio, index_ligne_valeur_sortante, pivot):
     ratio[index_ligne_valeur_sortante] = ratio[index_ligne_valeur_sortante]/pivot
 
-
-# ratio, min_ratio, index_ligne_valeur_sortante, ligne_pivot = calcule_v_sortante(tab, quantite, colonne_pivot)
-
 # Etape 6 : Diviser la ligne du pivot par le pivot
 
 def mise_a_jour_ligne_pivot(tab,pivot,index_ligne_valeur_sortante,index_colonne_valeur_entrante,ligne_pivot, nom_coeff, vdb, cp):
@@ -355,8 +119,6 @@ def mise_a_jour_ligne_pivot(tab,pivot,index_ligne_valeur_sortante,index_colonne_
     quantite[index_ligne_valeur_sortante] = quantite[index_ligne_valeur_sortante]/pivot
     vdb[index_ligne_valeur_sortante] = nom_coeff[index_colonne_valeur_entrante]
     return cp, vdb, quantite, tab, nom_coeff, ligne_pivot
-
-# cp, vdb, quantite, tab, nom_coeff, ratio = mise_a_jour_ligne_pivot(tab, index_ligne_valeur_sortante,index_colonne_valeur_entrante,ligne_pivot, nom_coeff, ratio, vdb, cp)
 
 def cp_a_jour(cp, index_ligne_valeur_sortante, index_colonne_valeur_entrante):
     cp[index_ligne_valeur_sortante] = objectif[index_colonne_valeur_entrante]
@@ -373,7 +135,6 @@ def zj_a_jour(cp,tab,Zj, index_ligne_valeur_sortante):
         Zj[i] = 0
         for j in range(len(cp)):
             Zj[i] += cp[j] * tab[j][i]
-            # print(cp[j], "x", tab[j][i])
     return Zj
 
 def zj_cj_a_jour(cj, zj):
@@ -386,16 +147,8 @@ def calcule_autre_lignes(tab, colonne_pivot, ligne_pivot, index_ligne_valeur_sor
         if index_ligne != index_ligne_valeur_sortante:
             for index_colonne in range(len(tab[index_ligne])):
                 tab[index_ligne][index_colonne] = tab[index_ligne][index_colonne] - colonne_pivot[index_ligne] * ligne_pivot[index_colonne] 
-                # print( tab[index_ligne][index_colonne] ,"-", colonne_pivot[index_ligne] ,"*", ligne_pivot[index_colonne] )
             quantite[index_ligne] = quantite[index_ligne] - colonne_pivot[index_ligne] * quantite[index_ligne_valeur_sortante]  
-            # print(quantite[index_ligne], "-",colonne_pivot[index_ligne], "x",quantite[index_ligne_valeur_sortante] )
     return tab
-
-# tab = calcule_autre_lignes(tab, colonne_pivot, ligne_pivot, index_ligne_valeur_sortante)
-
-# df_tab0 = built_tab_simplex(cp, vdb, quantite, tab, nom_coeff, ratio) 
-
-# print_tab_simplex(df_tab0, "1ère itération")
 
 def simplexe(contraintes, inegalites, quantite):
     # Initialisation du premier
@@ -444,7 +197,5 @@ def simplexe(contraintes, inegalites, quantite):
                 print_tab_simplex(z,df_tab0, "Itération " + str(indicate), zj, Cj_Zj)
             else :
                 print_tab_simplex(z,df_tab0, "Tableau Final", zj, Cj_Zj)
-
-
 
 simplexe(contraintes, inegalites, quantite)
